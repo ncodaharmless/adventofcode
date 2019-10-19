43fee150f11c274cabd600ae37c29ea2b39faa6f
Jason Toms <ncoda@outlook.com>
2019-10-19 20:04:01 +1000

    day 25 part 1

---
+++ ConsoleApp1/Utils/Vector4.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Utils
+{
+    public class Vector4
+    {
+        public int X;
+        public int Y;
+        public int Z;
+        public int T;
+
+        public Vector4(int x,int y, int z, int t)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+            T = t;
+        }
+
+        public Vector4(string commaDelimited)
+        {
+            string[] split = commaDelimited.Trim().Split(',');
+            X = int.Parse(split[0]);
+            Y = int.Parse(split[1]);
+            Z = int.Parse(split[2]);
+            T = int.Parse(split[3]);
+        }
+
+        public Vector4()
+        {
+        }
+
+        public int ManhattanDistance(Vector4 b)
+        {
+            return Math.Abs(X - b.X) + Math.Abs(Y - b.Y) + Math.Abs(Z - b.Z) + Math.Abs(T - b.T);
+        }
+
+        public override string ToString()
+        {
+            return $"{X},{Y},{Z},{T}";
+        }
+    }
+}
