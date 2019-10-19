12cc2c5f706ba266269cc054ed35be3f5aa8eb29
Jason Toms <ncoda@outlook.com>
2019-10-19 13:43:30 +1000

    day 23 part 1

---
+++ ConsoleApp1/Utils/Vector3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Utils
+{
+    public class Vector3
+    {
+        public int X;
+        public int Y;
+        public int Z;
+
+
+        public int ManhattanDistance(Vector3 b)
+        {
+            return Math.Abs(X - b.X) + Math.Abs(Y - b.Y) + Math.Abs(Z - b.Z);
+        }
+
+    }
+}
