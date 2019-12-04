5472106c08120ccd58a7f4b1a438d2e063bc7bb6
Jason Toms <jastoms@gmail.com>
2019-12-04 21:22:54 +1000

    Day 3 and 4

---
+++ AdventOfCode/Utils/Line.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Utils
+{
+    class Line
+    {
+        public Point From { get; }
+        public Point To { get; }
+
+        public Line(Point from, Point to)
+        {
+            From = from;
+            To = to;
+        }
+
+    }
+
+    /// <summary>
+    /// Vertical or horizontal line only
+    /// We can cheat on the intersection tests, since they will only intersect if one is horiz and the other is vert
+    /// And the intersection is the vert line Y and horiz line X
+    /// </summary>
+    class AxisLine : Line
+    {
+        public static AxisLine Up(Point from, int distance)
+        {
+            return new AxisLine(from, new Point(from.X, from.Y - distance));
+        }
+
+        public static AxisLine Down(Point from, int distance)
+        {
+            return new AxisLine(from, new Point(from.X, from.Y + distance));
+        }
+
+        public static AxisLine Left(Point from, int distance)
+        {
+            return new AxisLine(from, new Point(from.X - distance, from.Y));
+        }
+
+        public static AxisLine Right(Point from, int distance)
+        {
+            return new AxisLine(from, new Point(from.X + distance, from.Y));
+        }
+
+        public AxisLine(Point from, Point to) : base(from, to)
+        {
+            if (from.X != to.X && from.Y != to.Y) throw new Exception("Not axis aligned");
+        }
+        public bool ContainsPoint(Point p)
+        {
+            return p.X >= Math.Min(From.X, To.X) && p.X <= Math.Max(From.X, To.X)
+                && p.Y >= Math.Min(From.Y, To.Y) && p.Y <= Math.Max(From.Y, To.Y);
+        }
+        public bool Intersects(AxisLine line)
+        {
+            if (IsHoriz == line.IsHoriz) return false;
+
+            Point point = Intersection(line);
+            return ContainsPoint(point) && line.ContainsPoint(point);
+        }
+
+        public int Length => Math.Abs(From.X - To.X) + Math.Abs(From.Y - To.Y);
+
+        public bool IsHoriz => From.Y == To.Y;
+
+        public Point Intersection(AxisLine line)
+        {
+            if (From.X == To.X) // this is vert, line is horiz
+                return new Point(From.X, line.From.Y);
+            // this is horiz, line is vert
+            return new Point(line.From.X, From.Y);
+        }
+    }
+}
---
+++ AdventOfCode/Year2019/Day04.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Year2019
+{
+
+
+    class Day04
+    {
+        #region Input
+        #endregion
+
+        public Day04()
+        {
+        }
+
+        public static bool IsValidPassword(int number, bool partB = false)
+        {
+            string str = number.ToString();
+            int adjacentCount = 0;
+            bool hasAdjacent = false;
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (str[i - 1] == str[i])
+                {
+                    adjacentCount++;
+                }
+                else
+                {
+                    if (partB)
+                    {
+                        if (adjacentCount == 1)
+                            hasAdjacent = true;
+                    }
+                    else if (adjacentCount > 0)
+                        hasAdjacent = true;
+                    adjacentCount = 0;
+                }
+                if (str[i - 1] > str[i])
+                    return false;
+            }
+            if (partB)
+            {
+                if (adjacentCount == 1)
+                    hasAdjacent = true;
+            }
+            else if (adjacentCount > 0)
+                hasAdjacent = true;
+            if (!hasAdjacent) return false;
+
+            return true;
+        }
+
+        internal int Part1()
+        {
+            int from = 130254;
+            int to = 678275;
+            int validCount = 0;
+            for (int i = from; i <= to; i++)
+                if (IsValidPassword(i))
+                    validCount++;
+            return validCount;
+        }
+
+        internal int Part2()
+        {
+            int from = 130254;
+            int to = 678275;
+            int validCount = 0;
+            for (int i = from; i <= to; i++)
+                if (IsValidPassword(i, partB: true))
+                    validCount++;
+            return validCount;
+        }
+    }
+
+    [TestClass]
+    public class TestDay04
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            Assert.IsTrue(Day04.IsValidPassword(111111));
+            Assert.IsFalse(Day04.IsValidPassword(223450));
+            Assert.IsFalse(Day04.IsValidPassword(123789));
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            Assert.IsTrue(Day04.IsValidPassword(112233, partB: true));
+            Assert.IsFalse(Day04.IsValidPassword(123444, partB: true));
+            Assert.IsTrue(Day04.IsValidPassword(111122, partB: true));
+        }
+
+        [TestMethod]
+        public void Part1()
+        {
+            Console.WriteLine(new Day04().Part1());
+        }
+        [TestMethod]
+        public void Part2()
+        {
+            Console.WriteLine(new Day04().Part2());
+        }
+    }
+
+}
